5b768b020e3c753fdc0464f36ff93381809eb159
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-03-07 13:49:43 +0100

    feat: marquee anim onscroll added to footer + cursorImagesTrails added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import styles from './Footer.module.scss'
 import { useRef } from "react";
 import Background from "../Background/Background";
 import { Marquee } from "../Marquee/Marquee";
+import { CursorImagesTrail } from "../CursorImagesTrail/CursorImagesTrail";
 
 interface FooterProps {
   blok: {
@@ -21,11 +22,16 @@ export const Footer = ({ blok, isLoading }: FooterProps) => {
   } else {
     return (
       <footer {...storyblokEditable(blok)} className={`grid-container ${styles.footer}`} ref={ref}>
-
-
-        <Marquee className={styles.marquee}>
+        <Marquee className={styles.marquee} container={ref.current}>
           <strong className={styles.marqueeText}>{blok.marquee}</strong>
         </Marquee>
+
+        <CursorImagesTrail blok={blok.cursorAnim} cursorArea={ref || null} />
+
+        <div className={styles.info}>
+          <p className={styles.job}>{blok.jobInfo}</p>
+          <p className={styles.school}>{blok.schoolInfo}</p>
+        </div>
       </footer>
     );
   }
--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -2,16 +2,20 @@
 import { useEffect, useRef } from 'react';
 import React from 'react';
 import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from './Marquee.module.scss'
+
 
 interface MarqueeProps {
   children : any,
   speed? : number,
   play?: boolean,
-  className?: string
+  className?: string,
+  container: any
 }
 
-export const Marquee = ({ children, speed = 0.07, play = true, className }: MarqueeProps) => {
+export const Marquee = ({ children, speed = 0.07, play = true, className, container }: MarqueeProps) => {
+  const marquee = useRef<HTMLDivElement>(null);
   const firstTextRef = useRef<HTMLElement[]>([]);
   const secondTextRef = useRef<HTMLElement[]>([]);
   let xPercent = 0;
@@ -19,13 +23,29 @@ export const Marquee = ({ children, speed = 0.07, play = true, className }: Marq
 
   useEffect(() => {
     if (play) {
+      gsap.registerPlugin(ScrollTrigger);
       requestAnimationFrame(animation)
+
+      gsap.to(marquee.current, {
+        scrollTrigger : {
+          trigger: container,
+          start: 'top top',
+          end: 'bottom bottom',
+          scrub: 0.25,
+          onUpdate: e => direction = e.direction * -1
+        },
+        x: '-=300px',
+      })
     }
   }, [play]);
 
   const animation = () => {
     if (xPercent <= -100) {
       xPercent = 0
+    }
+
+    if (xPercent > 0) {
+      xPercent = -100;
     }
 
     gsap.set(firstTextRef.current, {xPercent: xPercent})
@@ -36,7 +56,7 @@ export const Marquee = ({ children, speed = 0.07, play = true, className }: Marq
   }
 
   return (
-    <div className={`${styles.marquee} ${className}`}>
+    <div className={`${styles.marquee} ${className}`} ref={marquee}>
       { React.cloneElement(children, {ref: firstTextRef, className: `${children.props.className || ''} ${styles.text}`.trim()}) }
       { React.cloneElement(children, {ref: secondTextRef, className: `${children.props.className || ''} ${styles.text}`.trim()}) }
     </div>
