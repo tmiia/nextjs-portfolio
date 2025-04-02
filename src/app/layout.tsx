8e235ac31130372cc07d06c345e75a3588cdd59a
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-04-02 15:04:02 +0200

    feat: upgrade seo by adding openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,54 @@ const Dirtyline = localFont({
   variable: '--font-dirtyline',
 })
 
-
 export const metadata: Metadata = {
   title: "Nehémia Telusma • Front End Developer",
   description: "Nehémia Telusma, a Front End Developer based in Paris, looking for an apprenticeship.",
+  metadataBase: new URL('https://www.nehemiatelusma.com'),
+  keywords: ['Front End Developer', 'Web Development', 'React', 'NextJS', 'Paris', 'Developer Portfolio', 'Gobelins', 'Nehémia Telusma'],
+  authors: [{ name: 'Nehémia Telusma' }],
+  creator: 'Nehémia Telusma',
+  publisher: 'Nehémia Telusma',
+
+  openGraph: {
+    type: 'website',
+    title: 'Nehémia Telusma • Front End Developer',
+    description: "Nehémia Telusma, a Front End Developer based in Paris, looking for an apprenticeship.",
+    url: 'https://www.nehemiatelusma.com',
+    siteName: 'Nehémia Telusma Portfolio',
+    images: [
+      {
+        url: '/seo/seo_miiadev_portfolio.png',
+        width: 1200,
+        height: 630,
+        alt: 'Nehémia Telusma - Front End Developer Portfolio',
+      },
+    ],
+    locale: 'en_US',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Nehémia Telusma • Front End Developer',
+    description: 'Front End Developer based in Paris seeking apprenticeship opportunities',
+    images: ['/seo/seo_miiadev_portfolio.png'],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+  alternates: {
+    canonical: 'https://www.nehemiatelusma.com',
+  },
 };
 
 storyblokInit({
